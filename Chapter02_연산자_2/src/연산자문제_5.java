f668e563c5d7146054421691d6f6efea1c6f3dac
sist106 <sist106@DESKTOP-IQT80LE>
2024-04-16 18:12:27 +0900

    101141

--- Chapter02_연산자_2/src/연산자문제_5.java
+++ Chapter02_연산자_2/src/연산자문제_5.java
@@ -3,12 +3,12 @@ public class 연산자문제_5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	      int num1=10;   
-	      System.out.printf("%d \n", num1); //10
+	      int num1=10;     
+	      System.out.printf("%d \n", num1); //10 
 	      num1++; 
 	      System.out.printf("%d \n", num1); //11
 	      ++num1; 
-	      System.out.printf("%d \n", num1); //12
+	      System.out.printf("%d \n", num1); //12 
 	      --num1;
 	      System.out.printf("%d \n", num1); //11
 	      num1--; 
