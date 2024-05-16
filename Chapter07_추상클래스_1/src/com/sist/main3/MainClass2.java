17600960464b7ef7626ff709b6d18b7cdbed6b17
sist106 <sist106@DESKTOP-IQT80LE>
2024-05-16 14:02:45 +0900

    0516

--- Chapter07_추상클래스_1/src/com/sist/main3/MainClass2.java
+++ Chapter07_추상클래스_1/src/com/sist/main3/MainClass2.java
@@ -17,7 +17,7 @@ import java.awt.event.*;
  * 
  * 
  */
-public class MainClass2 extends JFrame implements MovieListner,KeyListener,Runnable{
+public class MainClass2 extends JFrame implements MouseListener,KeyListener,Runnable{
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -44,6 +44,36 @@ public class MainClass2 extends JFrame implements MovieListner,KeyListener,Runna
 
 	@Override
 	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
