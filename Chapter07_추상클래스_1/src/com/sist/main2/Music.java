1f3f239bfd77bdb12e4f817a0165436991e3a6ec
sist106 <sist106@DESKTOP-IQT80LE>
2024-05-16 16:45:41 +0900

    0516

--- Chapter07_추상클래스_1/src/com/sist/main2/Music.java
+++ Chapter07_추상클래스_1/src/com/sist/main2/Music.java
@@ -14,7 +14,7 @@ public class Music {
 	}
 	public void setMno(String mno) {
 		// 임시 변수 => 메소드 안에서만 사용, 다른 클래스에서는 사용 불가 
-		this.mno = mno;
+		this.mno = mno; 
 		// this는 자신의 클래스 객체 주소를 저장해서 사용 
 		// this는 static 모든 객체는 this를 가지고 있다 
 		// this가 많이 사용되는 곳 => 멤버변수/지역변수의 이름이 같은 경우에 구분 
