b131bcddad4c886e8344926e56ab1bb0980263ef
sist106 <sist106@DESKTOP-IQT80LE>
2024-05-22 18:09:33 +0900

    0522 라이브러리(Math,StringBuffer,System) 정리

---
+++ Chapter09_라이브러리(java.lang)/src/com/sist/lang/Math_StringBuffer_System.java
@@ -0,0 +1,26 @@
+package com.sist.lang;
+/*
+ *	Math : 수학 => 모두 static 메소드이다 
+ *   = random() => Math.random() => 임의의 수를 추출 // 클래스 이름으로 접근 
+ *     public static double random()
+ *     : 0.0~0.99 사이 숫자 출력 
+ *   = ceil() => Math.ceil () => 올림 // 총 페이지
+ *     public static double ceil(double d)
+ *     
+ *  StringBuffer : String 저장시에 사용되는 클래스 (최적화, 속도) 
+ *   = append() => 문자열 결합
+ *     public void append(String s)
+ *   = toString() => 저장된 모든 문자열을 변환
+ *     public String toString()
+ *    
+ *  System: 운영체제 관련 
+ *   = print(), println(), printf() => 출력
+ *   = gc() => 필요없는 객체 메모리 회수 
+ *   = exit() => 프로그램 종료 
+ */
+public class Math_StringBuffer_System {
+
+	public static void main(String[] args) {
+
+	}
+}
