0471f5464c70e18e751e67481c5a21935e51cd0f
sist106 <sist106@DESKTOP-IQT80LE>
2024-05-23 17:54:13 +0900

    0523

---
+++ Chapter10_라이브러리(java.util)_2/src/com/sist/util/컬렉션_정리_02.java
@@ -0,0 +1,26 @@
+package com.sist.util;
+/*
+ *	라이브러리 
+ *   => 클래스 : 하는 역할
+ *      ----
+ *      => 메소드 : 원형 (리턴형/매개변수)
+ *         ---- 어떤 기능을 수행하는지
+ *              ----------------
+ *              프로그램에서 사용하는 위치
+ *  ------------------------------------
+ *  => 사용처
+ *  ----------- 프로그램 : 라이브러리 + 사용자 정의 (메소드, 클래스)
+ *   java.lang / java.util / java.text / java.io / java.net / java.sql
+ *   ---------
+ *    Object / String / StringBuffer / Math / Wrapper 
+ *                                            Integer / Double / Boolean
+ *                                            
+ */
+public class 컬렉션_정리_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
